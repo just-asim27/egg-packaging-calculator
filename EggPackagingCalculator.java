a18669ed8027824b8f890dd4a60a73cb0f2d0380
Asim <139613262+just-asim27@users.noreply.github.com>
2025-09-06 23:28:09 +0500

    Add files via upload

---
+++ EggPackagingCalculator.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class EggPackagingCalculator {
+
+    public static void main(String[] args) {
+
+        Scanner userInput = new Scanner(System.in);
+
+        // Get total eggs; determines number of cartons needed
+        System.out.print("Enter total number of eggs: ");
+        int totalEggs = userInput.nextInt();
+
+        // Standard carton sizes, largest first
+        int[] cartonSizes = {30, 24, 18, 12, 6};
+
+        System.out.println("\nEgg Packaging Plan:");
+
+        int remainingEggs = totalEggs; 
+        int totalCartons = 0;          
+
+        for (int size : cartonSizes) {
+
+            int cartons = remainingEggs / size;  // Number of cartons of this size
+            remainingEggs = remainingEggs % size;   // Eggs left for smaller cartons
+
+            if (cartons > 0) {
+
+                // Print carton size and quantity
+                System.out.printf("%d-egg cartons: %d%n", size, cartons);
+                totalCartons += cartons;
+
+            }
+
+        }
+
+        // Remaining eggs that don’t fit any carton
+        System.out.printf("Leftover eggs: %d%n", remainingEggs);
+
+        // Total cartons used
+        System.out.printf("Total cartons: %d%n", totalCartons);
+
+        userInput.close();
+
+    }
+
+}
